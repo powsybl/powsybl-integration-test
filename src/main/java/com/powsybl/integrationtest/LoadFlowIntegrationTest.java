9ff8a22cad510aa7917ae22926de3c55a5cfe34b
Bertrand Rix <bertrand.rix@artelys.com>
2022-03-22 12:15:11 +0100

    Clean after copy paste, add headers and fix code smell.
    Signed-off-by: Bertrand Rix <bertrand.rix@artelys.com>

--- src/main/java/com/powsybl/integrationtest/LoadFlowIntegrationTest.java
+++ src/main/java/com/powsybl/integrationtest/LoadFlowIntegrationTest.java
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2022, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.powsybl.integrationtest;
 
 import com.powsybl.iidm.network.Network;
@@ -8,6 +14,9 @@ import com.powsybl.loadflow.LoadFlowResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * @author Bertrand Rix <bertrand.rix at artelys.com>
+ */
 public final class LoadFlowIntegrationTest {
     private static final Logger LOGGER = LoggerFactory.getLogger(LoadFlowIntegrationTest.class);
 
@@ -19,5 +28,6 @@ public final class LoadFlowIntegrationTest {
         LoadFlowParameters parameters = new LoadFlowParameters().setNoGeneratorReactiveLimits(true)
                                              .setDistributedSlack(false);
         LoadFlowResult result = LoadFlow.run(network, parameters);
+        LOGGER.info("Load flow result isOk : {}.", result.isOk());
     }
 }
